509fe8d8ce201b92184598e8d38d4ddd67ca7691
ztosyl <ztsapos@gmail.com>
2020-05-31 13:51:25 -0400

    Updates

---
+++ precache-manifest.483246d9bb6d0aac804bbe5240d884c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f6f6260188b3ac9c180fedab360241",
+    "url": "/zambetas-react-app/index.html"
+  },
+  {
+    "revision": "54a298832092501f6fd0",
+    "url": "/zambetas-react-app/static/css/main.5313967e.chunk.css"
+  },
+  {
+    "revision": "1cc9683ed2a595b206e3",
+    "url": "/zambetas-react-app/static/js/2.5c5dce75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zambetas-react-app/static/js/2.5c5dce75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a298832092501f6fd0",
+    "url": "/zambetas-react-app/static/js/main.af955303.chunk.js"
+  },
+  {
+    "revision": "118200c5d6d91c7f19f7",
+    "url": "/zambetas-react-app/static/js/runtime-main.59fe74b1.js"
+  }
+]);
